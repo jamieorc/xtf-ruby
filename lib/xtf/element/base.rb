7b9e41e436b071b7a554e94b15b67a018ca9b27f
Jamie Orchard-Hays <jamieorc@dang-mbp2.local>
2007-12-12 22:52:03 -0500

    converted base to use proper instance attributes

--- lib/xtf/element/base.rb
+++ lib/xtf/element/base.rb
@@ -1,13 +1,22 @@
 class XTF::Element::Base
+  ATTRIBUTE_KEYS = [:field, :max_snippets, :boost]
   
-  attr_accessor :attributes
-  attr_reader :tag_name
+  ATTRIBUTE_KEYS.each { |k| attr_accessor k }
+  attr_reader   :tag_name
   
   def initialize(*args)
-    @attributes = {}
     params = args[0] || {}
-    params = params.symbolize_keys
-    @attributes.merge!(params)
+    params.each_pair { |k, v| self.__send__("#{k}=", v) }
+  end
+  
+  def attribute_keys
+    ATTRIBUTE_KEYS
+  end
+  
+  # Returns a +Hash+ of the attributes listed in +ATTRIBUTE_KEYS+ with their values.
+  # The keys are +Symbol+s.
+  def attributes
+    self.attribute_keys.inject({}) { |hash, key| hash[key] = self.__send__(key); hash }
   end
 
 end
---
+++ spec/base_spec.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+include XTF::Element
+
+describe Base do
+  before(:each) do
+    @attributes = {:field => "name", :max_snippets => "6", :boost => "2"}
+    @base = XTF::Element::Base.new(@attributes)
+  end
+
+  it "should have 'ATTRIBUTE_KEYS' array with 'field', 'max_snippets', 'boost'" do
+    XTF::Element::Base::ATTRIBUTE_KEYS.length.should == 3
+    XTF::Element::Base::ATTRIBUTE_KEYS.should == [:field, :max_snippets, :boost]
+  end
+  
+  it "should have 'attribute_keys' equal to ATTRIBUTE_KEYS" do
+    @base.attribute_keys.should == XTF::Element::Base::ATTRIBUTE_KEYS
+  end
+  
+  it "'attributes' should return the attributes listed in ATTRIBUTE_KEYS in a Hash with their values" do
+    @base.attributes.size.should == 3
+    @base.attributes.should == @attributes
+  end
+  
+  it "should take attributes as a Hash when instantiated and populate each property." do
+    @base.field.should == @attributes[:field]
+    @base.max_snippets.should == @attributes[:max_snippets]
+    @base.boost.should == @attributes[:boost]
+  end
+end
