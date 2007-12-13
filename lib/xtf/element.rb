e1b396342aa01b609c59ddfc450beea06b9a4f48
Jamie Orchard-Hays <jamieorc@dang-mbp2.local>
2007-12-13 11:11:24 -0500

    need to require base and clause before the other classes that extend them

--- lib/xtf/element.rb
+++ lib/xtf/element.rb
@@ -3,4 +3,7 @@ module XTF
   end
 end
 
+require File.dirname(__FILE__) + "/element/base.rb"
+require File.dirname(__FILE__) + "/element/clause.rb"
+
 Dir[File.dirname(__FILE__) + "/element/*.rb"].each { |file| require(file) }
