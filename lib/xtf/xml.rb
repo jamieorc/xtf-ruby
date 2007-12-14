08596c2bf2ab440568eaa2d5b64cb180e6661ef0
Jamie Orchard-Hays <jamieorc@dang-mbp2.local>
2007-12-14 17:05:47 -0500

    added NOTICE.txt and Solr-Ruby attribution

--- lib/xtf/xml.rb
+++ lib/xtf/xml.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Code in this file is derived from Apache's Solr-Ruby project.
+
 module XTF::XML
 end
 
