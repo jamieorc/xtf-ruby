8848e3ee435204ab2f4bc897c75d05c7c6bf72d6
Jamie Orchard-Hays <jamieorc@dang-mbp2.local>
2008-01-18 15:11:48 -0500

    added new AllDocs class--for facet searches

---
+++ spec/all_docs_spec.rb
@@ -0,0 +1,28 @@
+# Copyright 2007 James (Jamie) Orchard-Hays
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require File.dirname(__FILE__) + '/spec_helper'
+
+include XTF::Element
+
+describe "AllDocs.new" do
+  it "should take no arguments and raise error if any passed in" do
+    lambda { AllDocs.new }.should_not raise_error(ArgumentError)
+    lambda { AllDocs.new(:value => "none") }.should raise_error(ArgumentError)    
+  end
+  
+  it "'to_xml' should only return '<allDocs/>" do
+    AllDocs.new.to_xml.should == "<allDocs/>"
+  end
+end
