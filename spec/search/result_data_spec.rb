527eb58d6c07a60b8617e62315ffb16fab54a200
Jamie Orchard-Hays <jamieorc@dang-mbp-2.local>
2008-09-29 13:34:03 -0400

    refactored search element specs into new search folder

--- spec/search/result_data_spec.rb
+++ spec/search/result_data_spec.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 include XTF::Element
 
